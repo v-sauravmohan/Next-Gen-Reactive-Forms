ab763bbb7b00378d6559965aad102fa8e9e5899e
Saurav Mohan V <48208603+xcaliber5553@users.noreply.github.com>
2019-03-26 16:46:49 +0530

    Minor Changes
    Added cancel button to contact us page.
    Cancel Button Css added to style of respective page.
    Router navigate fuction for when cancel button is clicked also added but not wired.

--- src/app/ContactUsForm/contactus.component.ts
+++ src/app/ContactUsForm/contactus.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
 templateUrl : './contactus.template.html',
 styleUrls: ['./contactus.style.css']
 })
-export class ContactUs {}
+export class ContactUs {
+    constructor(private router:Router){}
+    cancelBtn = function(){
+        this.router.navigate('/');
+    }
+}
