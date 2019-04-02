c743558cad5527e9533848fbeb79d2e1b9ada933
Saurav Mohan V <48208603+xcaliber5553@users.noreply.github.com>
2019-04-02 18:07:23 +0530

    Update contactus.component.ts
    Added Form Controls to all text Fields in the form.

--- src/app/ContactUsForm/contactus.component.ts
+++ src/app/ContactUsForm/contactus.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormControl} from '@angular/forms';
+import {FormControl,FormGroup} from '@angular/forms';
+import { FnParam } from '@angular/compiler/src/output/output_ast';
+import { Subject } from 'rxjs';
 
 @Component({
 templateUrl : './contactus.template.html',
@@ -8,7 +10,12 @@ styleUrls: ['./contactus.style.css']
 })
 
 export class ContactUs {
-
+    contactUsForm = new FormGroup({
+        fname: new FormControl(''),
+        lname: new FormControl(''),
+        country: new FormControl(''),
+        subject: new FormControl('')
+    })
     
     constructor(private router:Router){}
     cancel(){
