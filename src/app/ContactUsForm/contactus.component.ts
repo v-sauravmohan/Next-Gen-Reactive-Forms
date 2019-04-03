856160f3d67df4a3d1fa0871d10e2fea093d5607
Saurav Mohan V <48208603+xcaliber5553@users.noreply.github.com>
2019-04-03 12:41:49 +0530

    Revert "Revert "Revert "Update contactus.component.ts"""
    This reverts commit f2bea2b8aa63a29fbe40265f651d57f10852b821.

--- src/app/ContactUsForm/contactus.component.ts
+++ src/app/ContactUsForm/contactus.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormControl,FormGroup} from '@angular/forms';
-import { FnParam } from '@angular/compiler/src/output/output_ast';
-import { Subject } from 'rxjs';
+import {FormControl} from '@angular/forms';
 
 @Component({
 templateUrl : './contactus.template.html',
@@ -10,12 +8,7 @@ styleUrls: ['./contactus.style.css']
 })
 
 export class ContactUs {
-    contactUsForm = new FormGroup({
-        fname: new FormControl(''),
-        lname: new FormControl(''),
-        country: new FormControl(''),
-        subject: new FormControl('')
-    })
+
     
     constructor(private router:Router){}
     cancel(){
