5c62213416a1c897c4b198d7433b966fc7d2c3e5
Saurav Mohan V <48208603+xcaliber5553@users.noreply.github.com>
2019-04-03 18:27:44 +0530

    Form Validation on Contact Us
    Form Validation on Contact us Working in the back end ie reactive side but not working in the front end ie to display the errors to the user part.

--- src/app/ContactUsForm/contactus.component.ts
+++ src/app/ContactUsForm/contactus.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormControl} from '@angular/forms';
+import {FormControl,FormBuilder,FormGroup,Validators} from '@angular/forms';
+
 
 @Component({
 templateUrl : './contactus.template.html',
 styleUrls: ['./contactus.style.css']
 })
 
-export class ContactUs {
-
+export class ContactUs implements OnInit {
+contactUsForm:FormGroup
+fnameCtrl: FormControl
+lnameCtrl: FormControl
+countryCtrl: FormControl
+subjectCtrl: FormControl
+constructor(private router:Router,private fb:FormBuilder){}
     
-    constructor(private router:Router){}
-    cancel(){
-        this.router.navigate(['/']);
+    ngOnInit(){
+        this.fnameCtrl = this.fb.control('',[Validators.required,Validators.minLength(2)]);
+        this.lnameCtrl = this.fb.control('',[Validators.required,Validators.minLength(2)]);
+        this.countryCtrl = this.fb.control('',[Validators.required]);
+        this.subjectCtrl = this.fb.control('',[Validators.required,Validators.minLength(10)]);
+        this.createForm();
+
+        
+}
+    
+    createForm(){
+    this.contactUsForm = this.fb.group({
+            fname: this.fnameCtrl,
+            lname: this.lnameCtrl,
+            country: this.countryCtrl,
+            subject: this.subjectCtrl
+    })       
+    }
+    
+    OnSubmit(){
+//for Testing -> Display values entered to the form in the console.
+    console.log(this.contactUsForm.value);
+}
+    OnCancel(){
+    this.router.navigate(['/']);
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { Register } from './RegisterForm/regf.component';
 import { ContactUs } from './ContactUsForm/contactus.component';
 import { NavBar } from './Navbar/navbar.component';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {FormsModule,ReactiveFormsModule} from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
